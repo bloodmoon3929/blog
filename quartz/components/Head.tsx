e0269c47a1a63183101e9ff0b4b4c1979606402f
bloodmoon3929 <gnbupi@naver.com>
2025-04-01 15:06:20 +0900

    Head에 meta 태그

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -53,6 +53,9 @@ export default (() => {
         <link rel="preconnect" href="https://cdnjs.cloudflare.com" crossOrigin="anonymous" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
+
+        <meta name="google-site-verification" content="XN7PJO0NnqOBflBD3-yfQ-0A20u5Eped-LoZ3kbNmjQ" />
+
         <meta name="og:site_name" content={cfg.pageTitle}></meta>
         <meta property="og:title" content={title} />
         <meta property="og:type" content="website" />
