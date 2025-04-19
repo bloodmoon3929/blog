97a80c0c4c5cf833016a5cbdf78e6f68a76b30fd
bloodmoon3929 <gnbupi@naver.com>
2025-04-19 20:44:22 +0900

    정상화

--- quartz/components/Body.tsx
+++ quartz/components/Body.tsx
@@ -7,28 +7,7 @@ import googleAdsScript from "./scripts/googleads.inline"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 
 const Body: QuartzComponent = ({ children }: QuartzComponentProps) => {
-  return (
-    <div id="quartz-body">
-      {children}
-      <script dangerouslySetInnerHTML={{ __html: `
-        // SPA 내비게이션 재정의
-        document.addEventListener('nav', function(e) {
-          // 내비게이션 이벤트에서 URL 가져오기
-          const targetUrl = e.detail.url;
-          console.log('Navigation to', targetUrl, 'detected');
-          
-          // 약간의 지연 후 강제 새로고침
-          setTimeout(function() {
-            // 현재 위치와 비교하여 변경 사항이 있는 경우에만 새로고침
-            if (location.pathname !== '/' + targetUrl) {
-              console.log('Forcing page refresh for ads to work properly');
-              location.href = '/' + targetUrl;
-            }
-          }, 100);
-        });
-      ` }}></script>
-    </div>
-  )
+  return <div id="quartz-body">{children}</div>
 }
 
 Body.afterDOMLoaded = `
