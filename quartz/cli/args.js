93a1e44fedd72b2c55ddb33c255b1ad7d11b9fdb
bloodmoon3929 <gnbupi@naver.com>
2025-04-24 16:19:19 +0900

    살짝 짜치는 해결

--- quartz/cli/args.js
+++ quartz/cli/args.js
@@ -2,7 +2,7 @@ export const CommonArgv = {
   directory: {
     string: true,
     alias: ["d"],
-    default: "src/site/notes",
+    default: "src/site",
     describe: "directory to look for content files",
   },
   verbose: {
