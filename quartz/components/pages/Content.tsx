96aa9fe2be709fa33e8c279325930ff6f6e09873
bloodmoon3929 <gnbupi@naver.com>
2025-04-24 14:25:09 +0900

    중복된 광고호출 제거

--- quartz/components/pages/Content.tsx
+++ quartz/components/pages/Content.tsx
@@ -8,17 +8,15 @@ const Content: QuartzComponent = ({ fileData, tree }: QuartzComponentProps) => {
   const classString = ["popover-hint", ...classes].join(" ")
   return <article class={classString}>
     {content}
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9389715933657453"
-     crossorigin="anonymous"></script>
-<ins class="adsbygoogle"
-     style="display:block; text-align:center;"
-     data-ad-layout="in-article"
-     data-ad-format="fluid"
-     data-ad-client="ca-pub-9389715933657453"
-     data-ad-slot="9562619076"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
+    <ins class="adsbygoogle"
+        style="display:block; text-align:center;"
+        data-ad-layout="in-article"
+        data-ad-format="fluid"
+        data-ad-client="ca-pub-9389715933657453"
+        data-ad-slot="9562619076"></ins>
+      <script>
+          (adsbygoogle = window.adsbygoogle || []).push({});
+      </script>
     </article>
 }
 
