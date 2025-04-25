bfbf59743d8ed2f9a4faa97cee2ba08479f00079
bloodmoon3929 <gnbupi@naver.com>
2025-04-25 22:01:21 +0900

    이미지 수정

--- quartz/components/image.ts
+++ quartz/components/image.ts
@@ -1,13 +1,15 @@
+/** @jsx h */
+import { h } from "preact"
 import { QuartzComponent, QuartzComponentConstructor } from "./types"
 import { classNames } from "../util/lang"
 import { pathToRoot } from "../util/path"
 
 const Image: QuartzComponent = ({ fileData, displayClass }) => {
-  const baseDir = pathToRoot(fileData.slug!) // pageTitle과 같은 방식으로 root path로 돌아가기
+  const baseDir = pathToRoot(fileData.slug!)
 
   return (
     <img
-      src="/icon.png" // 고정된 아이콘 경로
+      src="/static/icon" // 고정된 아이콘 경로
       alt="아이콘 설명"
       class={classNames(displayClass, "image")}
       onClick={() => window.location.href = baseDir} // 클릭 시 페이지 이동
