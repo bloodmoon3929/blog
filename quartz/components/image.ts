f16576061b68e9e69bcec9ed91b49703996dbccd
bloodmoon3929 <gnbupi@naver.com>
2025-04-25 22:05:40 +0900

    jsx 말고 html 형식으로

--- quartz/components/image.ts
+++ quartz/components/image.ts
@@ -1,30 +1,25 @@
-/** @jsx h */
-import { h } from "preact"
 import { QuartzComponent, QuartzComponentConstructor } from "./types"
 import { classNames } from "../util/lang"
 import { pathToRoot } from "../util/path"
 
 const Image: QuartzComponent = ({ fileData, displayClass }) => {
   const baseDir = pathToRoot(fileData.slug!)
+  const iconPath = "/static/icon" // 아이콘 경로 설정
 
-  // Quartz에서 경로 문제를 피하려면 올바른 경로 지정이 필요
-  const iconPath = "/static/icon" // /static 폴더 내 경로로 아이콘 설정
-
-  return (
+  return `
     <img
-      src={iconPath} // 고정된 아이콘 경로
+      src="${iconPath}" 
       alt="아이콘 설명"
-      class={classNames(displayClass, "image")}
-      onClick={() => window.location.href = baseDir} // 클릭 시 페이지 이동
-    />
-  )
+      class="${classNames(displayClass, 'image')}"
+      onclick="window.location.href='${baseDir}'" />
+  `
 }
 
 Image.css = `
 .image {
   cursor: pointer;
-  width: 50px; /* 원하는 크기로 설정 */
-  height: 50px; /* 원하는 크기로 설정 */
+  width: 50px; /* 원하는 크기 */
+  height: 50px; /* 원하는 크기 */
   border-radius: 50%;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
 }
