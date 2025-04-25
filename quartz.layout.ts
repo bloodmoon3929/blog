aed34d12b805f8d17a84917d28245ccf33bf3eb4
bloodmoon3929 <gnbupi@naver.com>
2025-04-26 00:05:31 +0900

    악

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -40,8 +40,16 @@ export const defaultContentPageLayout: PageLayout = {
     Component.TagList(),
   ],
   left: [
-    Component.PageTitle(),
-    Component.Image(),
+    Component.Flex({
+      components: [
+        {
+          Component: Component.PageTitle(),
+        },
+        { Component: Component.Image() },
+      ],
+    }),
+    //Component.PageTitle(),
+    //Component.Image(),
     Component.MobileOnly(Component.Spacer()),
     Component.Flex({
       components: [
