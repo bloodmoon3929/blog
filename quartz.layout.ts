a378c2cc7b559a22643f5096cfa3513a696acdae
bloodmoon3929 <gnbupi@naver.com>
2025-04-26 01:22:15 +0900

    중앙으로

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -50,8 +50,8 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(
       Component.Flex({
         components: [
-          { Component: Component.Image() },
-          { Component: Component.PageTitle()},
+          { Component: Component.Image() ,align : "center"},
+          { Component: Component.PageTitle(),align : "center"},
         ],
       })
     ),
