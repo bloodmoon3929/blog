77c198df3d4545dfa59c0ceb2f20b768eb3780b6
bloodmoon3929 <gnbupi@naver.com>
2025-04-25 22:26:04 +0900

    살려줘

--- quartz/components/image.ts
+++ quartz/components/image.ts
@@ -1,6 +1,7 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import { classNames } from "../util/lang"
 import { pathToRoot } from "../util/path"
+import { h } from "preact" // Preact의 createElement 함수
 
 const Image: QuartzComponent = ({ fileData, displayClass }: QuartzComponentProps) => {
   const baseDir = pathToRoot(fileData.slug!)
@@ -8,21 +9,19 @@ const Image: QuartzComponent = ({ fileData, displayClass }: QuartzComponentProps
   const iconPath = "quartz/static/icon.png" 
 
 
-  return (
-    <img
-      src={iconPath}
-      alt="개발하는 사카밤바피스"
-      class={classNames(displayClass, 'image')}
-      onClick={() => window.location.href = baseDir}
-    />
-  )
+  return h('img', {
+    src: iconPath,
+    alt: "개발하는 사카밤바피스",
+    class: classNames(displayClass, 'image'),
+    onClick: () => window.location.href = baseDir
+  })
 }
 
 Image.css = `
 .image {
   cursor: pointer;
-  width: 50px; /* 원하는 크기 */
-  height: 50px; /* 원하는 크기 */
+  width: 50px; 
+  height: 50px; 
   border-radius: 50%;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
 }
