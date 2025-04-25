03d6759ad0802886148c3cd1c864e208e475ea69
bloodmoon3929 <gnbupi@naver.com>
2025-04-25 21:37:17 +0900

    닉네임 변경
    SACAP!S

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "BloodMoon3929",
+    pageTitle: "SACAP!S",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
